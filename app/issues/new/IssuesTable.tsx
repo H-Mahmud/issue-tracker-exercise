99035eb7f71f87eb06fee209ff0ba0d6bc72a347
Mahmudul Hasan <me@imahmud.com>
2024-01-07 18:30:10 +0600

    Refactor: Extract the IssuesTable

---
+++ app/issues/new/IssuesTable.tsx
@@ -0,0 +1,43 @@
+import { Table } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+const IssuesTable = async () => {
+  const issues = await prisma.issue.findMany();
+  return (
+    <Table.Root variant='surface'>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className='hidden md:table-cell'>
+            Status
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className='hidden md:table-cell'>
+            Created At
+          </Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => {
+          return (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className='block md:hidden'>
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.createdAT.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssuesTable;
