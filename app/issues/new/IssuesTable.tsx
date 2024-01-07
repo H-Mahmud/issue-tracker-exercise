d36753649e1b29aca0c9e47097d51a508f8e405a
Mahmudul Hasan <me@imahmud.com>
2024-01-07 19:42:33 +0600

    Build styled Link component

--- app/issues/new/IssuesTable.tsx
+++ app/issues/new/IssuesTable.tsx
@@ -1,7 +1,7 @@
 import { Table } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import Link from 'next/link';
+import Link from '@/app/components/Link';
 
 const IssuesTable = async () => {
   const issues = await prisma.issue.findMany();
