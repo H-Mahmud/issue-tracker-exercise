d6d42c8c2fd076d8f0fcb914409af0244c3bbc96
Mahmudul Hasan <me@imahmud.com>
2024-01-08 15:16:00 +0600

    Configure Google Provider

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,6 +1,13 @@
+import envVars from '@/env';
 import { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 const authOptions: AuthOptions = {
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: envVars.GOOGLE_CLIENT_ID,
+      clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 };
 export default authOptions;
