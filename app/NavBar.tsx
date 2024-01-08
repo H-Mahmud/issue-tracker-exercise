43662a0ed692304788b470fa4b11839d29348904
Mahmudul Hasan <me@imahmud.com>
2024-01-08 20:46:04 +0600

    Refactor: Extract Navbar components

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,74 +8,15 @@ import { IconContext } from 'react-icons';
 import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
-  const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issue', href: '/issues' },
-  ];
-
-  const currentPath = usePathname();
-  const { status, data } = useSession();
-
   return (
     <nav className='border-b '>
       <Container>
         <Flex justify='between' align='center' className='h-20 px-5'>
           <Flex>
-            <Link href='/' className='fill-gray-700 mr-3'>
-              <IconContext.Provider
-                value={{
-                  size: '2em',
-                  className: 'fill-gray-700 hover:fill-gray-900',
-                }}
-              >
-                <AiFillBug />
-              </IconContext.Provider>
-            </Link>
-            <ul className='flex items-center space-x-6'>
-              {links.map((link, index) => {
-                return (
-                  <li key={index}>
-                    <Link
-                      className={classNames({
-                        'text-zinc-95000': currentPath === link.href,
-                        'text-zinc-600 hover:text-zinc-700':
-                          currentPath !== link.href,
-                        'transition-colors': true,
-                      })}
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+            <SiteLogo />
+            <NavigationLinks />
           </Flex>
-          <Box>
-            {status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={data.user?.image!}
-                    fallback='?'
-                    size='3'
-                    radius='full'
-                    className='cursor-pointer'
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>{data.user?.email}</DropdownMenu.Label>
-                  <DropdownMenu.Separator />
-                  <DropdownMenu.Item color='red' onClick={() => signOut()}>
-                    Logout
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === 'unauthenticated' && (
-              <Link href='/api/auth/signin'>Login</Link>
-            )}
-          </Box>
+          <UserOptions />
         </Flex>
       </Container>
     </nav>
@@ -83,3 +24,83 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+const SiteLogo = () => {
+  return (
+    <Link href='/' className='fill-gray-700 mr-3'>
+      <IconContext.Provider
+        value={{
+          size: '2em',
+          className: 'fill-gray-700 hover:fill-gray-900',
+        }}
+      >
+        <AiFillBug />
+      </IconContext.Provider>
+    </Link>
+  );
+};
+
+const NavigationLinks = () => {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issue', href: '/issues' },
+  ];
+
+  const currentPath = usePathname();
+
+  return (
+    <ul className='flex items-center space-x-6'>
+      {links.map((link, index) => {
+        return (
+          <li key={index}>
+            <Link
+              className={classNames({
+                'transition-colors nav-link': true,
+                '!text-zinc-950': currentPath === link.href,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const UserOptions = () => {
+  const { status, data } = useSession();
+
+  if (status === 'loading') return null;
+
+  if (status === 'unauthenticated')
+    return (
+      <Link href='/api/auth/signin' className='nav-link'>
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={data!.user?.image!}
+            fallback='?'
+            size='3'
+            radius='full'
+            className='cursor-pointer'
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>{data!.user?.email}</DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item color='red' onClick={() => signOut()}>
+            Logout
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
